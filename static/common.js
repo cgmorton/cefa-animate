58f8d448892221c9ff0e75b1f7fd8174e6f4f5ba
charles morton <charles.morton@dri.edu>
2017-02-13 14:48:59 -0800

    Moving non map elements to common.js
    Also wired up forward and back buttons to move the slider positions to
    the start and end.

---
+++ static/common.js
@@ -0,0 +1,58 @@
+// Hardcoding the Fire Danger values, colors, and labels here for now
+var legend_title = 'CEFA Hourly Fire Danger';
+var fd_values = ['1', '2', '3', '4', '5', '-99', '0'];
+var fd_colors = {
+    '1': '#00ff00',
+    '2': '#0000ff',
+    '3': '#ffff00',
+    '4': '#ffa500',
+    '5': '#ff0000',
+    '-99': '#bebebe',
+    '0': '#ffffff',
+};
+var fd_labels = {
+    '1': 'Low [1]',
+    '2': 'Medium [2]',
+    '3': 'High [3]',
+    '4': 'Very High [4]',
+    '5': 'Extreme [5]',
+    '-99': 'Missing [-99]',
+    '0': 'No Staff Val [0]',
+}
+var fd_nodata = -99;
+
+
+// Load the test data from the JSON file
+// This could eventually be moved to a dedicated "data" script
+var featureData;
+$.getJSON('/data/data.json', function(json) {
+    featureData = json;
+});
+
+
+// GeoJSON data
+var featureNameField = 'FDRA'
+var featureGeoJSON = '/data/zonemap2008.geojson';
+
+
+// Initialize the slider
+var default_i = 0;
+var min_i = 0;
+var max_i = 11;
+$('#slider').slider({value: default_i, min: min_i, max: max_i, step: 1,});
+$('#slider').on('slidechange', function( event, ui ) {
+    setFeatureStyle(ui.value);
+    $('#timeLabel').html(ui.value)} );
+
+
+// Link the control buttons to the slider
+$('#step-backward-btn').click(function() {
+    // This sets slider to beginning
+    $('#slider').slider("value", min_i)
+});
+
+$('#step-forward-btn').click(function() {
+    // This sets slider to the end
+    $('#slider').slider("value", max_i)
+});
+
--- static/googlemaps.js
+++ static/googlemaps.js
@@ -1,123 +1,28 @@
-function mapInit() {
-    var mapCenter = {lat: 37.5, lng: -120};
-    var mapZoom = 6;
+var map;
 
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: mapZoom,
-        center: mapCenter,
+function mapInit() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 6,
+        center: {lat: 37.5, lng: -120},
         mapTypeId: 'satellite',
         //mapTypeId: 'terrain',
         streetViewControl: false
     });
 
-    // Hardcoding the Fire Danger values, colors, and labels here for now
-    var legend_title = 'CEFA Hourly Fire Danger';
-    var fd_values = ['1', '2', '3', '4', '5', '-99', '0'];
-    var fd_colors = {
-        '1': '#00ff00',
-        '2': '#0000ff',
-        '3': '#ffff00',
-        '4': '#ffa500',
-        '5': '#ff0000',
-        '-99': '#bebebe',
-        '0': '#9acd32',
-    };
-    var fd_labels = {
-        '1': 'Low [1]',
-        '2': 'Medium [2]',
-        '3': 'High [3]',
-        '4': 'Very High [4]',
-        '5': 'Extreme [5]',
-        '-99': 'Missing [-99]',
-        '0': 'No Staff Val [0]',
-    }
-    var fd_nodata = -99;
-
-
     // Read in feature geometries from the GeoJSON file
-    var featureNameField = 'FDRA'
-    //map.data.loadGeoJson('/data/zonemap2008.geojson');
-    map.data.loadGeoJson('/data/zonemap2008.geojson', { idPropertyName: featureNameField });
-    //map.data.loadGeoJson('/data/zonemap2008.geojson', { 'idPropertyName': featureNameField });
-    //var feature = map.data.getFeatureById(508);
-    //console.log(feature.getProperty('FDRA'));
-    //console.log(feature.getProperty('POINTS'));
+    map.data.loadGeoJson(featureGeoJSON, { idPropertyName: featureNameField });
 
+    // Wait for the request to complete by listening for the first feature to be added
+    // https://developers.google.com/maps/documentation/javascript/combining-data
+    google.maps.event.addListenerOnce(map.data, 'addfeature', function() {
+        setFeatureStyle(default_i);
+    });
+    // Apply initial styling to the features
+    //setFeatureStyle(default_i);
 
-    // Set the control box (slider, play, pause, etc.) on the map
+    // Position the control box (slider, play, pause, etc.) on the map
     var controls = document.getElementById('controls');
     map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(controls);
-
-
-    // Set the default slider position
-    var default_i = 0
-    // Set the slider control
-    $( function() {
-        $('#slider').slider({value: default_i, min: 0, max: 11, step: 1});
-    });
-
-
-    function setFeatureStyle(slider_i) {
-        map.data.setStyle(function setStyle(feature){
-            var fdra = feature.getProperty(featureNameField);
-
-            // If the value is not set, use the default value
-            //if (fdra in featureData) {
-            if (fdra in featureData & slider_i in featureData[fdra]) {
-                var ftr_color = fd_colors[featureData[fdra][slider_i]];
-            } else {
-                var ftr_color = fd_colors[fd_nodata];
-            }
-
-            return {
-                fillColor: ftr_color,
-                fillOpacity: 0.80,
-                strokeWeight: 0.5
-            };
-        });
-    };
-
-
-    // Link the slider to the features
-    $('#slider').on('slide', function( event, ui ) {
-        $('#timeLabel').html(ui.value);
-        setFeatureStyle(ui.value)
-    });
-
-
-    // Read feature values from a separate json file
-    // Set default feature style from values
-    var featureData;
-    $.getJSON('/data/data.json', function(json) {
-        featureData = json;
-        setFeatureStyle(default_i)
-    });
-
-
-    // // Update the feature styling based on the slider position
-    // document.getElementById('slider').addEventListener('input', function(e) {
-    //     slider_i = parseInt(e.target.value, 10);
-    //     var timeLabel = document.getElementById('timeLabel')
-    //     timeLabel.textContent = slider_i;
-    //     map.data.setStyle(function setStyle(feature){
-    //         var fdra = feature.getProperty(featureNameField);
-
-    //         // If the value is not set, use the default value
-    //         //if (fdra in featureData) {
-    //         if (fdra in featureData & slider_i in featureData[fdra]) {
-    //             var ftr_color = fd_colors[featureData[fdra][slider_i]];
-    //         } else {
-    //             var ftr_color = fd_colors[fd_nodata];
-    //         }
-
-    //         return {
-    //             fillColor: ftr_color,
-    //             fillOpacity: 0.80,
-    //             strokeWeight: 0.5
-    //         };
-    //     });
-    // });
-
 
     // Set the legend colors and labels
     var legend = document.getElementById('legend');
@@ -134,7 +39,6 @@ function mapInit() {
     }
     map.controls[google.maps.ControlPosition.RIGHT_TOP].push(legend);
 
-
     // Update lower left info-box when a user clicks on a region
     // Eventually try making info box move to region/clock location?
     map.data.addListener('click', function(event) {
@@ -143,7 +47,6 @@ function mapInit() {
         fdra = event.feature.getProperty('FDRA')
         document.getElementById('info-box').textContent = fdra + '\r\n' + fdra;
     });
-
 
     // Highlight each region on mouseover
     map.data.addListener('mouseover', function(event) {
@@ -156,7 +59,6 @@ function mapInit() {
         //map.data.overrideStyle(event.feature, {strokeWeight: 0.5});
     });
 
-
     // Write data to an InfoWindow that pops up on the clicked feature
     // This is currently using the clicked position
     // I think it would look nicer using pre-computed feature centroids
@@ -168,7 +70,6 @@ function mapInit() {
         infoWindow.open(map);
     });
 
-
     // Keep map position centered when window resizes
     google.maps.event.addDomListener(window, 'resize', function() {
         var center = map.getCenter();
@@ -178,6 +79,20 @@ function mapInit() {
 }
 
 
-//google.maps..event.addDomListener(window, 'load', initialize);
+function setFeatureStyle(slider_i) {
+    map.data.setStyle(function (feature) {
+        var fdra = feature.getProperty(featureNameField);
 
-
+        // If the value is not set, use the default value
+        if (fdra in featureData & slider_i in featureData[fdra]) {
+            var ftr_color = fd_colors[featureData[fdra][slider_i]];
+        } else {
+            var ftr_color = fd_colors[fd_nodata];
+        }
+        return {
+            fillColor: ftr_color,
+            fillOpacity: 0.80,
+            strokeWeight: 0.5
+        };
+    });
+};
